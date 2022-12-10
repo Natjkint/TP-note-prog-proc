65c979a3ad7d9d01b57f50e88d854ba66eb351c8
Natjkint <118830607+Natjkint@users.noreply.github.com>
2022-12-10 18:09:19 +0100

    Add files via upload

---
+++ Exo 1/fonctionsreutilisees.h
@@ -0,0 +1,39 @@
+#ifndef fonctionsreutilisees
+#define fonctionsreutilisees
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#define filename "airbnb_donnees_propre.csv"
+
+
+typedef struct logement {
+    int index;
+    int accommodates;
+    int bedrooms;
+    int bathrooms;
+    int beds;
+    int price;
+    int minimum_nights;
+    int maximum_nights;
+    int number_of_reviews;
+    float distance[7];
+} logement;
+
+logement LogementATester();
+
+int nbrLignes();
+
+logement copyTableau();
+
+int carre(float x);
+
+void shuffle(logement tab[], int taille);
+
+int moyennePrix(logement tab[], int k);
+
+int triinsertion(logement tab[]);
+
+#endif
