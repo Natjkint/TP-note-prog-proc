709871e9738307c92d0dfd6758af9990db5d33ef
C-Iaens <111684368+C-Iaens@users.noreply.github.com>
2022-12-11 13:40:50 +0000

    Update typedef.h

--- Exo2/typedef.h
+++ Exo2/typedef.h
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h> //sleep pour linux
+//#include <windows.h>     //sleep pour windows
 
 typedef struct processus{char str_nom[20];int priorite;int duree_exec;} processus;
-typedef struct ordonnanceur{int file[20]; processus proc[20];} ordonnanceur;
+typedef struct ordonnanceur{int file; processus proc[20];} ordonnanceur;
 
 ordonnanceur entree_processus(ordonnanceur A);
 ordonnanceur run(ordonnanceur A);
