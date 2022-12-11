a91a398b89685a7e78d0fc8aa33666f7160e6a09
C-Iaens <111684368+C-Iaens@users.noreply.github.com>
2022-12-11 10:58:44 +0000

    Update entree_processus.c

--- Exo2/entree_processus.c
+++ Exo2/entree_processus.c
@@ -1,6 +1,6 @@
 #include "typedef.h"
 
-ordonnanceur entree_processus() { 
+ordonnanceur entree_processus(ordonnanceur A) {
     processus procX;
     printf("Veuillez donner une activité : \n nom:");
     scanf("%s",procX.str_nom);
@@ -8,11 +8,12 @@ ordonnanceur entree_processus() {
     scanf("%d",&procX.priorite);
     printf("durée d'exécution:\n");
     scanf("%d",&procX.duree_exec);
-    ordonnanceur A;
     for (int i = 0; i < 20; ++i){
-        if(A.file[i]==0){
+        if(A.proc[i].priorite==-1){
             A.proc[i]=procX;
             return A;
         }
     }
+    printf("La file est pleine\n");
+    exit(EXIT_FAILURE);
 }
