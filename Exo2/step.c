14cf50b52ea0d492c090ffc857dbf057a23c1cf2
C-Iaens <111684368+C-Iaens@users.noreply.github.com>
2022-12-11 13:39:31 +0000

    Update step.c

--- Exo2/step.c
+++ Exo2/step.c
@@ -10,16 +10,16 @@ ordonnanceur step(ordonnanceur A){
         for (int i = 0; i < 20; ++i){
             if (A.proc[i].duree_exec != -1){
                 //"execute le processus"
-                printf("Processus %s, de priorité %d, et de durée d'exécution %d", A.proc[i].str_nom, A.proc[i].priorite, A.proc[i].duree_exec);
+                printf("Processus %s, de priorité %d, et de durée d'exécution %d\n", A.proc[i].str_nom, A.proc[i].priorite, A.proc[i].duree_exec);
 
-                printf("On attend %ds, le temps que le processus s'exécute !",A.proc[i].duree_exec);
+                printf("On attend %ds, le temps que le processus s'exécute !\n",A.proc[i].duree_exec);
                 sleep(A.proc[i].duree_exec);
 
                 A.proc[i].priorite=-1; A.proc[i].duree_exec=-1; memset(&A.proc[i].str_nom, 0, sizeof(A.proc->str_nom)); 
                 A.file--;       //On reset l'entrée du processus aux valeurs initiales.
-
                 return A;
             }
         }
     }
+    exit(EXIT_FAILURE);
 }
