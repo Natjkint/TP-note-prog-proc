c6d4e9cdfbd98843b988b189d0cfee5c3d9fb860
C-Iaens <111684368+C-Iaens@users.noreply.github.com>
2022-12-11 11:00:12 +0000

    Update step.c

--- Exo2/step.c
+++ Exo2/step.c
@@ -1,17 +1,25 @@
 #include "typedef.h"
 
 
-int step(ordonnanceur A){
-    for (int i = 0; i < 20; ++i){
-        if(A.file[i]!=0){
-            //"execute le processus"
-            printf("Processus %s, de priorité %d, et de durée d'exécution %d", A.proc[i].str_nom, A.proc[i].priorite, A.proc[i].duree_exec);
-            printf("On attend %ds, le temps que le processus s'exécute !",A.proc[i].duree_exec);
-            sleep(A.proc[i].duree_exec);
-            A.file[i]=0;    //A.str_nom[20][i]=NULL;A.priorite[i]=0;A.duree_exec[i]=0;
-            return 0;
+ordonnanceur step(ordonnanceur A){
+    if (A.file == 0){
+        printf("Il n'y a pas de processus en attente");
+        return A;
+    }
+    else{
+        for (int i = 0; i < 20; ++i){
+            if (A.proc[i].duree_exec != -1){
+                //"execute le processus"
+                printf("Processus %s, de priorité %d, et de durée d'exécution %d", A.proc[i].str_nom, A.proc[i].priorite, A.proc[i].duree_exec);
+
+                printf("On attend %ds, le temps que le processus s'exécute !",A.proc[i].duree_exec);
+                sleep(A.proc[i].duree_exec);
+
+                A.proc[i].priorite=-1; A.proc[i].duree_exec=-1; memset(&A.proc[i].str_nom, 0, sizeof(A.proc->str_nom)); 
+                A.file--;       //On reset l'entrée du processus aux valeurs initiales.
+
+                return A;
+            }
         }
     }
-    printf("Il n'y a pas de processus en attente");
-    return 10;
 }
