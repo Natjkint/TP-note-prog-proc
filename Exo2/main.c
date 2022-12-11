2198b85f9dfebfb5c7a6d25af7ece2a91fb0a17d
C-Iaens <111684368+C-Iaens@users.noreply.github.com>
2022-12-11 13:37:51 +0000

    Update main.c

--- Exo2/main.c
+++ Exo2/main.c
@@ -1,18 +1,24 @@
 #include "typedef.h"
 
 int main(){
-    ordonnanceur A;
+    ordonnanceur A;         //Initialisation
+    A.file=0;
+    for (int i = 0; i < 20; ++i){
+        A.proc[i].priorite=-1; A.proc[i].duree_exec=-1; memset(&A.proc[i].str_nom, 0, sizeof(A.proc->str_nom));
+    }
     int reponse2 = 0;
     while (reponse2==0){
         printf("Voulez-vous ajouter un processus ? : (oui / non) \n");
-        char str_reponse[3];
+        char str_reponse[20];
         scanf("%s", str_reponse);
         reponse2 = strcmp("oui",str_reponse);
         if (reponse2==0){
-            A = entree_processus();
+            A = entree_processus(A);
         }
     }
-    
     run(A);
-    return 0;
+    printf("est-ce que ça execute ça?");
+    if (A.file == 0){               //Dernier test
+        exit(EXIT_SUCCESS);
+    } else {exit(EXIT_FAILURE);}
 }
